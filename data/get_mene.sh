efecfce1b469d89f171d6bd2e97ad5dba85952ba
pablohewson <paul.hewson@cityscience.com>
2019-04-08 19:39:07 +0100

    Import MENE survey data to PostgreSQl
    Bash script importer for MENE survey data
    
    Working on MENE SAE

---
+++ data/get_mene.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# associative arrays are not POSIX standard, so disable shellcheck warning
+# shellcheck disable=SC2039
+
+set -e
+
+BASEURL=http://publications.naturalengland.org.uk/file/
+# TARGETPATH=$DATAROOT/SourceData/Observations/NaturalEngland/MENE/
+TARGETPATH=/home/phewson/mene/
+declare -A filenumbers
+
+filenumbers[6252849495539712]=excel_spss_mene_weighting_guide.pdf
+filenumbers[5701857678983168]=mene_year_1-9_respondent_data.csv
+filenumbers[5561617467310080]=mene_year_1_to_7_visit_data.csv
+filenumbers[6088843157831680]=mene_year_8_and_9_visit_data.csv
+filenumbers[4607962547224576]=nci_year_5_core_and_boost_data.csv
+filenumbers[5749422113947648]=mene_year_1_to_5_childrens_data.csv
+
+
+
+for file in "${!filenumbers[@]}"; do
+    wget -nc $BASEURL$file -P "$TARGETPATH"
+    ln -s "$TARGETPATH$file" "$TARGETPATH${filenumbers[$file]}"
+done
+
+
+
