8fdd48e8f7c274b1bc5c4a7c8a114264dbb2a3ce
pablohewson <paul.hewson@cityscience.com>
2019-04-30 14:30:18 +0100

    Finalise eda section, align with modelling
    The EDA section is finalised in terms of checking table formatting
    and wording. Greater clarity on the match between variables available
    and used in the modelling, specifically around the choice of years based
    on data collection
    
    Working on #6

--- R/sanitize_mene.R
+++ R/sanitize_mene.R
@@ -1,21 +1,24 @@
+library(dplyr)
+
+handle_nas <- function(x){
+    return(droplevels(addNA(factor(na_if(x, " "), exclude=NULL))))}
 
 mene_factors <- function(mene.df) {
-  mene.df$age = as.factor(mene.df$age)
-  mene.df$sex = as.factor(mene.df$sex)
-  mene.df$seg = as.factor(mene.df$seg)
-  mene.df$segall = as.factor(mene.df$segall)
-  mene.df$tenure = as.factor(mene.df$tenure)
-  mene.df$ethnicity = as.factor(mene.df$ethnicity)
-  mene.df$disability = as.factor(mene.df$disability)
-  mene.df$marstat = as.factor(mene.df$marital)
-  mene.df$adults_in_hh <- as.factor(mene.df$adults_in_hh)
-  mene.df$child_in_hh <- as.factor(mene.df$child_in_hh)
-  mene.df$hh_size <- as.factor(mene.df$hh_size)
-  mene.df$workstat <- as.factor(mene.df$workstat)
-  mene.df$car <- as.factor(mene.df$car)
-  mene.df$genhealth <- factor(mene.df$generalhealth, levels = c("Don't know", "Very bad", "Bad", "Fair", "Good", "Very good"))
-  mene.df$car[!(mene.df$car == "Yes" | mene.df$car == "No")] <- NA
-  mene.df$genhealth[mene.df$genhealth == "Don't know"] <- NA
+  mene.df$age = handle_nas(mene.df$age)
+  mene.df$sex = handle_nas(mene.df$sex)
+  mene.df$seg = handle_nas(mene.df$seg)
+  mene.df$segall = handle_nas(mene.df$segall)
+  mene.df$tenure = handle_nas(mene.df$tenure)
+  mene.df$ethnicity_5 = handle_nas(sapply(mene.df$ethnicity, mene_ethnicity_5))
+  mene.df$ethnicity = handle_nas(mene.df$ethnicity)
+  mene.df$disability = handle_nas(mene.df$disability)
+  mene.df$marstat = handle_nas(mene.df$marital)
+  mene.df$adults_in_hh <- handle_nas(mene.df$adults_in_hh)
+  mene.df$child_in_hh <- handle_nas(mene.df$child_in_hh)
+  mene.df$hh_size <- handle_nas(mene.df$hh_size)
+  mene.df$workstat <- handle_nas(mene.df$workstat)
+  mene.df$car <- handle_nas(mene.df$car)
+  mene.df$genhealth <- addNA(factor(mene.df$generalhealth, levels = c("Don't know", "Very bad", "Bad", "Fair", "Good", "Very good"), , exclude=NULL))
   return(mene.df)
 }
 
@@ -36,3 +39,23 @@ mene_code_q17 <- function(mene.df){
   mene.df$q17 <- factor(mene.df$q17, levels=c("Never", "Once or twice", "Once every 2-3 months", "Once or twice a month", "Once a week", "Several times a week", "Every day", "More than once per day"))
   return(mene.df)
 }
+
+mene_ethnicity_5 <- function(x){
+    if (x ==  "White British") {
+            y <- "white_british"
+        } else if (x == "Any other white background" ||
+                   x == "White Irish") {
+            y <- "white_other"
+        } else if (x == "African" || x == "Any other Black background" ||
+                   x == "Caribbean") {
+            y <- "black"
+        } else if (x == "Any other Asian backgrund" ||
+                   x == "Bangladeshi" || x == "Chinese" ||
+                   x == "Indian" || x == "Pakistani") {
+            y <- "asian"
+        }  else {
+            y <- "other"
+        }
+    return(y)}
+
+  
